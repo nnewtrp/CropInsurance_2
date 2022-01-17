2378c02a9fa48840469661b2a79a1f54ae889cff
zombiedude91 <newnack2013@gmail.com>
2022-01-17 23:05:29 +0700

    Update Form 2

--- pages/Form/index.vue
+++ pages/Form/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="12" md="7" class="my-10">
+  <v-layout row>
+    <v-btn
+      text
+      cols="12"
+      md="2"
+      class="my-16 mr-2 text-h5 font-weight-bold d-none d-md-block"
+      to="/"
+    >
+      <v-icon left>fa-arrow-circle-left</v-icon>
+      Home
+    </v-btn>
+    <v-col cols="12" sm="12" md="10" class="my-8">
       <ReportForm />
     </v-col>
-  </v-row>
+  </v-layout>
 </template>
 
 <script>
