21a3269c072333d0994e639f3760d9a05b2725a8
zombiedude91 <newnack2013@gmail.com>
2022-01-17 21:40:54 +0700

    add Form Page

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -5,6 +5,7 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _382bffe6 = () => interopDefault(import('..\\pages\\AllReport\\index.vue' /* webpackChunkName: "pages/AllReport/index" */))
+const _13576eb5 = () => interopDefault(import('..\\pages\\Form\\index.vue' /* webpackChunkName: "pages/Form/index" */))
 const _f058de4e = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
@@ -22,6 +23,10 @@ export const routerOptions = {
     path: "/AllReport",
     component: _382bffe6,
     name: "AllReport"
+  }, {
+    path: "/Form",
+    component: _13576eb5,
+    name: "Form"
   }, {
     path: "/",
     component: _f058de4e,
---
+++ pages/Form/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="12" md="7" class="my-10">
+      <WelcomeCard />
+    </v-col>
+    <v-col cols="12" sm="12" md="5" class="my-10">
+      <AllReportCard />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import WelcomeCard from '~/components/WelcomeCard.vue'
+import AllReportCard from '~/components/AllReportCard.vue'
+
+export default {
+  components: {
+    WelcomeCard,
+    AllReportCard,
+  },
+}
+</script>
