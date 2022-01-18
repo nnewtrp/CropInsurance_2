58329082eea4ebeb788011fc010f6b3ca143d123
zombiedude91 <newnack2013@gmail.com>
2022-01-18 09:35:28 +0700

    Update Form 3

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -2,12 +2,12 @@ import { wrapFunctional } from './utils'
 
 export { default as AllReportCard } from '../..\\components\\AllReportCard.vue'
 export { default as WelcomeCard } from '../..\\components\\WelcomeCard.vue'
-export { default as FormReportForm } from '../..\\components\\Form\\ReportForm.vue'
 export { default as NavbarAvatarMenu } from '../..\\components\\navbar\\AvatarMenu.vue'
+export { default as FormReportForm } from '../..\\components\\Form\\ReportForm.vue'
 export { default as ReportList } from '../..\\components\\Report\\ReportList.vue'
 
 export const LazyAllReportCard = import('../..\\components\\AllReportCard.vue' /* webpackChunkName: "components/all-report-card" */).then(c => wrapFunctional(c.default || c))
 export const LazyWelcomeCard = import('../..\\components\\WelcomeCard.vue' /* webpackChunkName: "components/welcome-card" */).then(c => wrapFunctional(c.default || c))
-export const LazyFormReportForm = import('../..\\components\\Form\\ReportForm.vue' /* webpackChunkName: "components/form-report-form" */).then(c => wrapFunctional(c.default || c))
 export const LazyNavbarAvatarMenu = import('../..\\components\\navbar\\AvatarMenu.vue' /* webpackChunkName: "components/navbar-avatar-menu" */).then(c => wrapFunctional(c.default || c))
+export const LazyFormReportForm = import('../..\\components\\Form\\ReportForm.vue' /* webpackChunkName: "components/form-report-form" */).then(c => wrapFunctional(c.default || c))
 export const LazyReportList = import('../..\\components\\Report\\ReportList.vue' /* webpackChunkName: "components/report-list" */).then(c => wrapFunctional(c.default || c))
