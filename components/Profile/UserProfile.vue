f7ccfc522d970614dfaf4ffc5b356ceb69370527
zombiedude91 <newnack2013@gmail.com>
2022-01-19 10:48:38 +0700

    Update Profile 1

--- components/Profile/UserProfile.vue
+++ components/Profile/UserProfile.vue
@@ -21,39 +21,7 @@
             <strong>Name:</strong> &ensp;{{ firstname }} {{ lastname }}
           </div>
           <div class="text--primary pb-4">
-            <strong>Username:</strong> &ensp;@{{ username }}
-          </div>
-          <div class="text--primary pb-4">
-            <strong>Gender:</strong> &ensp;{{ gender }}
-          </div>
-        </v-card-text>
-        <v-card-title class="headline">
-          <v-icon color="black">fa-map-marker-alt</v-icon>
-          &ensp;Workplace
-        </v-card-title>
-        <v-card-text class="subheading">
-          <div class="text--primary pb-4">
-            <strong>Country:</strong> &ensp;{{ country }}
-          </div>
-          <div class="text--primary pb-4">
-            <strong>Branch:</strong> &ensp;{{ branch }}
-          </div>
-          <div class="text--primary pb-4">
-            <strong>Department:</strong> &ensp;{{ department }}
-          </div>
-        </v-card-text>
-        <v-card-title class="headline">
-          <v-icon color="black">fa-address-card</v-icon>
-          &ensp;Contact
-        </v-card-title>
-        <v-card-text class="subheading">
-          <div class="text--primary pb-4">
-            <v-icon>fa-envelope</v-icon>
-            &nbsp;<strong>Email:</strong> &ensp;{{ email }}
-          </div>
-          <div class="text--primary pb-4">
-            <v-icon>fa-phone</v-icon>
-            &nbsp;<strong>Phone No:</strong> &ensp;{{ phone }}
+            <strong>Address:</strong> &ensp;{{ address }}
           </div>
         </v-card-text>
       </v-col>
@@ -74,13 +42,7 @@ export default {
       uid: '1',
       firstname: 'Teerapat',
       lastname: 'Satitporn',
-      username: 'newtrp',
-      gender: 'Male',
-      country: 'Thailand',
-      branch: 'Silom',
-      department: 'IT',
-      email: 'example@gmail.com',
-      phone: '0802534473',
+      address: 'Khlong Luang District, Pathumtani',
     }
   },
 }
