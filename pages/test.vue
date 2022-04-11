cbf33a240257ae5640f5c45f9a0b66fdbddb280f
zombiedude91 <newnack2013@gmail.com>
2022-04-11 18:56:37 +0700

    Create test.vue

---
+++ pages/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="text-align: center;">
+    <Logo/>
+    <h2>KokKok Hotel</h2>
+    <p>Users: {{ users }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    this.getUsers()
+  },
+  methods: {
+    async getUsers () {
+      let { data } = await this.$axios.get('/users')
+      if (!data.error) {
+        this.users = data.users
+      } else {
+        alert('ผิดพลาด ' + data.message)
+      }
+    }
+  }
+}
+</script>
