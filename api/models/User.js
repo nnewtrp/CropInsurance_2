604a51e6f4fcece36bf348fb0e6d6e41c832b475
zombiedude91 <newnack2013@gmail.com>
2022-04-16 18:47:42 +0700

    Update API models

--- api/models/User.js
+++ api/models/User.js
@@ -3,28 +3,16 @@ const sequelize = require('../config/db.connect')
 
 class User extends Model {}
 User.init({
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-  firstname: { type: DataTypes.STRING, allowNull: false },
-  lastname: { type: DataTypes.STRING, allowNull: false },
-  tel: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  created_at: { type: 'TIMESTAMP', defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), allowNull: false },
-  updated_at: { type: 'TIMESTAMP', defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), allowNull: false },
-  fullName: {
-    type: DataTypes.VIRTUAL,
-    get () {
-      return `${this.firstname} ${this.lastname}`
-    },
-    set (value) {
-      throw new Error('Do not try to set the `fullName` value!')
-    }
-  }
+  UserID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+  Email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  Password: { type: DataTypes.STRING, allowNull: false },
+  UserGroupID: { type: DataTypes.INTEGER, allowNull: false },
+  isActive: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
 }, {
   sequelize,
   modelName: 'User',
-  tableName: 'users',
-  indexes: [{ unique: true, fields: ['id'] }],
+  tableName: 'User',
+  indexes: [{ unique: true, fields: ['UserID'] }],
   defaultScope: {
     attributes: { exclude: ['password', 'token'] }
   },
---
+++ api/models/UserInfo.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/db.connect')
+
+class UserInfo extends Model {}
+UserInfo.init({
+  UserInfoID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+  UserID: { type: DataTypes.INTEGER, allowNull: false },
+  TitleName: { type: DataTypes.STRING, allowNull: false },
+  FirstName: { type: DataTypes.STRING, allowNull: false },
+  LastName: { type: DataTypes.STRING, allowNull: false },
+  PhoneNo: { type: DataTypes.STRING, allowNull: false },
+  ProfilePic: { type: DataTypes.STRING, allowNull: false, defaultValue: 'DefaultUserPicture.png' },
+  Province: { type: DataTypes.STRING, allowNull: false },
+  District: { type: DataTypes.STRING, allowNull: false },
+  SubDistrict: { type: DataTypes.STRING, allowNull: false },
+  MoreAddress: { type: DataTypes.STRING, allowNull: false },
+  LatLong: { type: DataTypes.STRING, allowNull: false },
+  fullName: {
+    type: DataTypes.VIRTUAL,
+    get () {
+      return `${this.firstname} ${this.lastname}`
+    },
+    set (value) {
+      throw new Error('Do not try to set the `fullName` value!')
+    }
+  }
+}, {
+  sequelize,
+  modelName: 'UserInfo',
+  tableName: 'UserInfo',
+  indexes: [{ unique: true, fields: ['UserInfoID'] }],
+})
+
+export default User
